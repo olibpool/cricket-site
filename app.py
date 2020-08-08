389a516ae2ebe443307e273b06f09dab88cfa034
Oli <oli.smithe@hotmail.co.uk>
2020-08-08 11:12:35 +0100

    syncing with laptop 1

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import sqlite3
 
-app = Flask(__name__)
+app: Flask = Flask(__name__)
 
 app.config['ENV'] = "development"
-app.config['DEBUG'] = 1
+app.config['DEBUG'] = True
+app.config['TESTING'] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return 'Hello World!'
+    if request.method == 'POST':
+        # Take the name that the user inputted:
+        name = request.form.get("name")
+        
+
+        # Connect to the stats database:
+        conn = sqlite3.connect("stats.db")
+        c = conn.cursor()
+
+        c.execute("SELECT :batorbowl")
+
+
+
+    else:
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
