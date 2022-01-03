dea686f9bd1ee7f1f74978869a7f6cc70ed994f0
olibpool <68647683+olibpool@users.noreply.github.com>
2022-01-03 13:17:11 +0000

    removed Talisman

--- app.py
+++ app.py
@@ -445,7 +445,7 @@ def rolling_page():
 
 
 # Wrap Flask app with Talisman
-Talisman(app, content_security_policy=None)
+#Talisman(app, content_security_policy=None)
 
 if __name__ == '__main__':
     app.run(debug=False)
